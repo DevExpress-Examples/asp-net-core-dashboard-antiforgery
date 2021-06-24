afba89e6226f7fff5ddff54d8274facb65ae891c
Natalia Kazakova (DevExpress) <natalia.kazakova@devexpress.com>
2021-06-24 13:20:57 +0300

    add cs solution

---
+++ CS/AspNetCoreDashboardPreventCrossSiteRequestForgery/Controllers/CustomDashboardController.cs
@@ -0,0 +1,41 @@
+﻿using DevExpress.DashboardAspNetCore;
+using DevExpress.DashboardWeb;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreDashboardPreventCrossSiteRequestForgery.Controllers
+{
+    [AutoValidateAntiforgeryToken]
+    public class CustomDashboardController : DashboardController
+    {
+        public CustomDashboardController(DashboardConfigurator configurator, IDataProtectionProvider dataProtectionProvider = null): base(configurator, dataProtectionProvider) { 
+        }
+
+    }
+
+
+    //public sealed class DashboardValidateAntiForgeryTokenAttribute : FilterAttribute, IAuthorizationFilter
+    //{
+    //    public void OnAuthorization(AuthorizationContext filterContext)
+    //    {
+    //        if (filterContext == null)
+    //        {
+    //            throw new ArgumentNullException(nameof(filterContext));
+    //        }
+
+    //        HttpContextBase httpContext = filterContext.HttpContext;
+    //        HttpRequestBase request = httpContext.Request;
+    //        HttpCookie cookie = request.Cookies[AntiForgeryConfig.CookieName];
+    //        string token = request.Headers["__RequestVerificationToken"];
+    //        if (string.IsNullOrEmpty(token))
+    //        {
+    //            token = request.Form["__RequestVerificationToken"];
+    //        }
+    //        AntiForgery.Validate(cookie?.Value, token);
+    //    }
+    //}
+}
